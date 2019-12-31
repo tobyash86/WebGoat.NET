681d5ac1297d01051616e911807d11f51d3c050a
Marek Rybak <mrybak@parasoft.com>
2019-12-31 09:23:58 +0100

    @req DT-14672 User should not see functional issues on WebGoat project @task DT-14697 Update error messages for required fields.

--- WebGoatCore/ViewModels/CheckoutViewModel.cs
+++ WebGoatCore/ViewModels/CheckoutViewModel.cs
@@ -8,32 +8,32 @@ namespace WebGoatCore.ViewModels
     public class CheckoutViewModel
     {
         [Display(Name = "Name to ship to")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter the name")]
         public string ShipTarget { get; set; }
 
         [Display(Name = "Address")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter address")]
         public string Address { get; set; }
 
         [Display(Name = "City")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter city")]
         public string City { get; set; }
 
         [Display(Name = "Region/State")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter region/state")]
         public string Region { get; set; }
 
         [Display(Name = "Postal Code")]
         [DataType(DataType.PostalCode)]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter postal code")]
         public string PostalCode { get; set; }
 
         [Display(Name = "Country")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter country")]
         public string Country { get; set; }
 
         [Display(Name = "Shipping method")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter country")]
         public int ShippingMethod { get; set; }
 
         [Display(Name = "Credit card number:")]
@@ -42,14 +42,14 @@ namespace WebGoatCore.ViewModels
         public string CreditCard { get; set; }
 
         [Display(Name = "Expiration")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter expiration month")]
         public int ExpirationMonth { get; set; }
 
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please enter expiration year")]
         public int ExpirationYear { get; set; }
 
         [Display(Name = "Remember this credit card number for next time I check out.")]
-        [Required(ErrorMessage = "adsasdasd")]
+        [Required(ErrorMessage = "Please mark whether credit card number should be remembered")]
         public bool RememberCreditCard { get; set; }
 
         public IDictionary<int, string> ShippingOptions { get; set; }
