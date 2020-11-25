09a764472c51c69bb0a44a2809d7689172152250
tobyash86 <tobyash@poczta.fm>
2020-11-25 12:18:39 +0100

    #4: Extending the list of handled HTTP reponse codes

--- WebGoatCore/ApiResponse.cs
+++ WebGoatCore/ApiResponse.cs
@@ -17,10 +17,26 @@ public class ApiResponse
     {
         switch (statusCode)
         {
+            case 400:
+                return "Bad Request.";
+            case 401:
+                return "Unauthorized: You do not have sufficient permissions to see this page.";
+            case 403:
+                return "Forbidden: Required permissions to view this page are not met.";
             case 404:
-                return "Resource not found";
+                return "Resource not found.";
+            case 408:
+                return "Request Timeout: Please retry accessing the resource later.";
+            case 410:
+                return "Gone: Resource has been removed and will not be available.";
+            case int i when i >= 400 && i <= 499:
+                return "Client Error: Please refer to the specified HTTP Status Code for more details.";
             case 500:
-                return "An unhandled error occurred";
+                return "Internal Server Error: An unhandled error occurred.";
+            case 501:
+                return "Not Implemented: This feature will be implemented soon.";
+            case int i when i >= 500 && i <= 599:
+                return "Server Error: Please refer to the specified HTTP Status Code for more details.";
             default:
                 return null;
         }
